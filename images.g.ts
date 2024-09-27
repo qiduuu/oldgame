0a618c572292099e433f4966c011ce9d5264c5ac
qiduuu <80431320+qiduuu@users.noreply.github.com>
2024-09-27 12:32:14 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,66 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "dragon":return img`
+...............7...............
+..............777..............
+..............f7f..............
+..............777..............
+..ddddd.......7d7.......ddddd..
+...7d77dd...ddd7ddd...dd77d7...
+....d7d77ddd77d7d77ddd77d7d....
+......d7d7d7d77d77d7d7d7d......
+........d7d7d.777.d7d7d........
+........d7d7d.7d7.d7d7d........
+..........d...777...d..........
+.............d7d7d.............
+.............d777d.............
+..............777..............
+...............7...............
+...............7...............
+..............d7d..............
+..............d7d..............
+...............7...............
+`;
+            case "image2":
+            case "fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 4 4 4 4 4 . . . . . . 
+. . . 4 4 4 5 5 5 5 4 4 4 4 . . 
+. . 4 5 5 5 5 5 5 5 5 5 4 4 . . 
+. . 4 5 5 1 1 1 5 5 5 5 5 4 . . 
+. 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
+. 4 5 5 1 1 5 5 5 1 1 5 5 5 4 . 
+. 4 5 5 1 1 5 1 1 5 5 5 5 5 4 . 
+. 2 5 5 5 5 1 1 1 5 1 1 5 5 2 . 
+. 2 5 5 5 5 1 1 1 5 1 1 5 5 2 . 
+. . 2 4 5 5 5 5 5 5 5 5 5 4 . . 
+. . . 2 2 4 5 5 5 5 5 4 4 . . . 
+. . 2 2 2 2 2 4 4 4 2 2 2 . . . 
+. . . 2 2 4 4 4 4 4 4 2 2 . . . 
+. . . . . 2 2 2 2 2 2 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
